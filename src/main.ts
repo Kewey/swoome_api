6cb1a6933644367d4d4c877c0e1e1d08bcd53248
Tom Siatka <tom.siatka@laposte.net>
2022-01-28 15:41:04 +0100

    add host

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   );
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0');
 }
 bootstrap();
