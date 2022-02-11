1d36ed215a714a4ba4890c75cb237c18378d1f5b
Tom Siatka <tom.siatka@laposte.net>
2022-02-11 17:58:00 +0100

    add group migration + crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { GroupModule } from './group/group.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, GroupModule],
 })
 export class AppModule {}
